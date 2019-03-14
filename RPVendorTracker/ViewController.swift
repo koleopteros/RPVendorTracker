a49f0faa871d8ddddf752306f71139f5c5dc414c
KoleosRemote <user151744@la221.local>
2019-03-14 08:48:15 -0700

    modified views

--- RPVendorTracker/ViewController.swift
+++ RPVendorTracker/ViewController.swift
@@ -8,13 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    let vendorsList = ["Bob Dylan", "Bob Ross", "Candy Dandy", "P. Pawluk", "Mike Denton", "Mark K."]
+    let itemsList = ["ball bearings", "Potion","15ft. Rope","Portable Chicken","Shoes","Iron Sword","Magical Hat"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return vendorsList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let myCell = tableView.dequeueReusableCell(withIdentifier: "ListCell", for: indexPath)
+        myCell.textLabel?.text = vendorsList[indexPath.row]
+        return myCell
+    }
+    
+    
+    @IBOutlet weak var segmentedCtrl: UISegmentedControl!
     override func viewDidLoad() {
+    
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func indexChanged(_ sender: Any) {
+    }
+    
 }
 
