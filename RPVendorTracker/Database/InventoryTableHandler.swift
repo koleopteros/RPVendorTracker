3a6cffe78b9b0b33b741d41761c437f8db29eab1
Tech <gbc@imac-76.local>
2019-04-04 13:55:01 -0400

    working on DB Helpers

--- RPVendorTracker/Database/InventoryTableHandler.swift
+++ RPVendorTracker/Database/InventoryTableHandler.swift
@@ -13,7 +13,7 @@ class InventoryTableHandler{
     let handler = DatabaseHandler()
     
     public func createInventoryTable(){
-        let query = "CREATE TABLE IF NOT EXISTS inventories (id INTEGER PRIMARY KEY AUTOINCREMENT, vendor_id INTEGER, FOREIGN KEY (vendor_id) REFERENCES vendors(id))"
+        let query = "CREATE TABLE IF NOT EXISTS inventory (vendors_id INTEGER NOT NULL REFERENCES vendors(id), items_id INTEGER NOT NULL REFERENCES items(id), quantity INTEGER NOT NULL, PRIMARY KEY (vendors_id, items_id)"
         
         if sqlite3_open(handler.fileURL.path, &db) != SQLITE_OK {
             print("failed to open db")
@@ -23,24 +23,9 @@ class InventoryTableHandler{
             print("error creating table: \(errmsg)")
         }
     }
-    public func createInventoryItemsTable(){
-        let query = "CREATE TABLE IF NOT EXISTS inventory_items (inventory_id INTEGER NOT NULL REFERENCES inventories(id), item_id INTEGER NOT NULL items(id), PRIMARY KEY (inventory_id, item_id)"
-        let handler = DatabaseHandler()
-        
-        if sqlite3_open(handler.fileURL.path, &db) != SQLITE_OK {
-            print("failed to open db")
-        }
-        if sqlite3_exec(db, query, nil, nil, nil) != SQLITE_OK {
-            let errmsg = String(cString: sqlite3_errmsg(db)!)
-            print("error creating table: \(errmsg)")
-        }
-    }
-    public func retrieveInventoryList(inventoryID:Int) -> [Int:Int] {
-        var list = [Int:Int]()
-        
-        let query = "SELECT * from inventory_items WHERE inventory_id = ?"
-        
-        return list
+    
+    public func retrieveInventory(vendor_id:Int){
+        let query = "SELECT items.name AS item_name, inventory.quantity AS quantity FROM inventory"
     }
 }
 
